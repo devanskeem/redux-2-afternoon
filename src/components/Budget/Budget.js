472dbee15db666a12461e7a160f51dc8eeff8693
Devan Skeem <devanskeem@gmail.com>
2019-05-29 16:47:20 -0600

    finished with step 5 start on step 6

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,16 +7,22 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import {connect} from 'react-redux'
+import {requestUserData} from '../../redux/userReducer'
 
 
 class Budget extends Component {
-
+  componentDidMount(){
+    this.props.requestUserData()
+  }
   render() {
+    const {loading} = this.props.budget
+    const {firstName, lastName} = this.props.user
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
@@ -33,4 +39,10 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps(reduxState){
+  return {
+    budget: reduxState.budget,
+    user: reduxState.user
+  }
+}
+export default connect(mapStateToProps, {requestUserData})(Budget);
---
+++ src/redux/budgetReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+function budgetReducer(state = initialState, action){
+    return state
+}
+
+export default budgetReducer
